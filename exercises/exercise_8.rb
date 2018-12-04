fc353339734f08e76462d9671f4f8e920c193848
Jack Bauer <bauer.jackw@gmail.com>
2018-12-04 14:33:44 -0800

    adds validation on store destory to prevent destroying store with employees

--- exercises/exercise_8.rb
+++ exercises/exercise_8.rb
@@ -7,6 +7,6 @@ require_relative './exercise_5'
 require_relative './exercise_6'
 require_relative './exercise_7'
 
-store = Store.find_by(id: 1)
-new_employee = Employee.create(first_name: 'Quiche', last_name: 'Hollandaise', hourly_rate: 50, store: store)
+@store1 = Store.find(1)
+new_employee = Employee.create(first_name: 'Quiche', last_name: 'Hollandaise', hourly_rate: 50, store: @store1)
 puts "Password: #{new_employee.password}"
--- lib/store.rb
+++ lib/store.rb
@@ -3,13 +3,15 @@ class Store < ActiveRecord::Base
     validates :name, length: { minimum: 3 }
     validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-    validate :must_have_either_mens_or_womens_apparel
+    validate errors.add(:base, "store must carry either men's or women's apparel") if !:mens_apparel && !:womens_apparel
+
+    before_destroy :check_if_empty?
 
     private
 
-    def must_have_either_mens_or_womens_apparel
-        if !mens_apparel && !womens_apparel
-            errors.add(:base, "store must carry either men's or women's apparel")
-        end
+    def check_if_empty?
+        return true if employees.count == 0
+        errors[:base] << "cannot destroy store with employees"
+        false
     end
 end
