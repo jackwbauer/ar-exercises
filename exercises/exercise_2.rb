a09319797f378e8989382430785e65e1c491871c
Jack Bauer <bauer.jackw@gmail.com>
2018-12-04 16:47:05 -0800

    fixes validation on mens and womens apparel

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -8,3 +8,4 @@ puts "----------"
 @store1 = Store.find_by(id: 1)
 @store2 = Store.find_by(id: 2)
 @store1.name = 'Kitsilano'
+@store1.save
--- lib/store.rb
+++ lib/store.rb
@@ -3,11 +3,15 @@ class Store < ActiveRecord::Base
     validates :name, length: { minimum: 3 }
     validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-    validate errors.add(:base, "store must carry either men's or women's apparel") if !:mens_apparel && !:womens_apparel
+    validate :has_mens_or_womens_apparel
 
     before_destroy :check_if_empty?
 
     private
+
+    def has_mens_or_womens_apparel
+        errors.add(:base, "store must carry either men's or women's apparel") unless mens_apparel || womens_apparel
+    end
 
     def check_if_empty?
         return true if employees.count == 0
